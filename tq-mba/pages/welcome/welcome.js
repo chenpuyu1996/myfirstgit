585663fb6d7a2905a43fb685c53ac934cebac503
chenpuyu <394316653@qq.com>
2019-02-26 09:48:01 +0800

    欢迎页面调试成功

--- tq-mba/pages/welcome/welcome.js
+++ tq-mba/pages/welcome/welcome.js
@@ -17,7 +17,7 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
     // 设置页面标题
     wx.setNavigationBarTitle({
       title: '太奇管理类联考'
@@ -35,21 +35,20 @@ Page({
   /**
    * 获取页面来源参数
    */
-  getentrance: function() {
+  getentrance: function () {
     //获取页面来源参数
     var entrance = wx.getLaunchOptionsSync()
     console.log(entrance)
     //判断本地缓存是否存在entrance
     if (entrance.query.projectId) {
       //获取到的entrance不为空则更新缓存
-      //console.log(entrance.query.projectId?true:false)
       wx.setStorageSync('entrance', entrance)
     }
   },
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
     intervalRun = true //定时器开
     // second = 2 //倒计时秒数，按需修改
     this.funCheckLogin() // 用户登录检测
@@ -57,7 +56,7 @@ Page({
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
     //清除定时器
     if (intervalRun) {
       intervalRun = false //定时器关
@@ -68,55 +67,69 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
     return {
       title: app.globalData.shareTitle,
       path: app.globalData.sharePath
     }
   },
   // 用户登录检测
-  funCheckLogin: function() {
+  funCheckLogin: function () {
     var _this = this
 
     try {
       var userInfo = wx.getStorageSync('userInfo') //读取用户本地信息
-      // console.log(userInfo)
+      console.log(userInfo)
       if (userInfo) {
         // 重新同步更新一次用户信息
-
+        //获取本地的 token
+        var token = wx.getStorageSync('token')
+        console.log(token)
         //在当前页面显示导航条加载动画
         wx.showNavigationBarLoading();
         // 异步重新获取一次用户数据，POST方法
         wx.request({
-          url: app.globalData.apiUrl + 'User/getUserInfo', //URL，按实际修改
-          method: "POST",
-          data: {
-            usrid: userInfo.usrid, //用户ID
-            token: userInfo.token, //用户验证token，本例为上次登录时间(毫秒)
+          url: app.globalData.apiUrl_two + 'wxmp/user/info/query?datatype=json', //URL，按实际修改
+          method: "GET",
+          header: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'UserToken': token
           },
-          success: function(res) {
+          success: function (res) {
             console.log(res.data)
             /**
              * 说明：
              * 限制用户惟一登录，如果上次登录时间未变，则接口重写并返回最新登录时间
              */
-            if (res.data.code == 0) {
-              userInfo = res.data.userInfo
+            if (res.data.success) {
+              userInfo = res.data.data
             } else {
               //用户自动异步登录失败
-              console.log(res.data.msg + ', CODE:' + res.data.code)
+              if (res.data.code == 100001) {
+                //清除缓存和全局变量
+                app.globalData.userInfo = null
+                wx.clearStorageSync()
+              } else {
+                //操作失败
+                wx.showToast({
+                  title: '操作失败！' + res.data.message,
+                  icon: 'none',
+                  duration: 4000
+                })
+              }
             }
 
             //更新本地存储用户信息
             try {
               wx.setStorageSync('userInfo', userInfo)
-            } catch (e) {}
+              wx.setStorageSync('token', res.data.data.upass)
+            } catch (e) { }
 
             //更新全局共享数据
             app.globalData.userInfo = userInfo
 
           },
-          complete: function(res) {
+          complete: function (res) {
             wx.hideNavigationBarLoading() //隐藏导航条加载动画
           }
         })
@@ -124,10 +137,10 @@ Page({
       } else {
         second = 1 //倒计时秒数，无本地存储信息1秒跳转
       }
-    } catch (e) {}
+    } catch (e) { }
 
     // 倒计时
-    interval = setInterval(function() {
+    interval = setInterval(function () {
       _this.setData({
         jumpSecond: second + '秒跳过',
       });
